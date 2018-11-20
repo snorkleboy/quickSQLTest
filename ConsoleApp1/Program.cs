ce02033dc756153e89ddb63c1a303850bab163aa
Artem Kharshan <akharshan@servicechannel.com>
2018-11-20 13:59:30 -0800

    cleaned

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,11 +12,11 @@ namespace ConsoleApp1
         {
             var testData = ValueMaker.makeValues(1000);
 
-            var SelectAllCommand = "SELECT * FROM arrayastabletest";
+            var selectAllCommand = "SELECT * FROM arrayastabletest";
             var sqlRunner = new SqlRunner();
             
-            new Tester(()=>sqlRunner.runQueryFull(SelectAllCommand), 10000, "SQL test read rows").test().printResult();
-            new Tester(()=>sqlRunner.runQuery(SelectAllCommand), 10000, "SQL test dont read rows").test().printResult();
+            new Tester(()=>sqlRunner.runQueryFull(selectAllCommand), 10000, "SQL test read rows").test().printResult();
+            new Tester(()=>sqlRunner.runQuery(selectAllCommand), 10000, "SQL test dont read rows").test().printResult();
 
             new Tester(() => {foreach(var str in testData){}}, 10000, "inMem Arr Test iterate").test().printResult();
             var testList = new List<string>();
