8c1db9dd7f295f6b912b81d4a8343e197b44d289
Artem Kharshan <akharshan@servicechannel.com>
2018-11-20 13:59:11 -0800

    cleaned

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,12 +15,12 @@ namespace ConsoleApp1
             var SelectAllCommand = "SELECT * FROM arrayastabletest";
             var sqlRunner = new SqlRunner();
             
-            new Tester(()=>sqlRunner.runQuery(SelectAllCommand), 10000, "SQL test").test().printResult();
-            new Tester(()=>sqlRunner.runQueryFull(SelectAllCommand), 10000, "SQL test").test().printResult();
+            new Tester(()=>sqlRunner.runQueryFull(SelectAllCommand), 10000, "SQL test read rows").test().printResult();
+            new Tester(()=>sqlRunner.runQuery(SelectAllCommand), 10000, "SQL test dont read rows").test().printResult();
 
-            new Tester(() => {foreach(var str in testData){}}, 10000, "inMem Arr Test").test().printResult();
+            new Tester(() => {foreach(var str in testData){}}, 10000, "inMem Arr Test iterate").test().printResult();
             var testList = new List<string>();
-            new Tester(() => {foreach(var str in testData){testList.Add(str+"a");}}, 10000, "inMem Arr Test + instantiate var").test().printResult();
+            new Tester(() => {foreach(var str in testData){testList.Add(str+"a");}}, 10000, "inMem Arr Test iterate + instantiate var").test().printResult();
 
             var selectCmd = "SELECT string FROM arrayastabletest where string = 'string800'";
             var selectCmdIndexed = "SELECT string FROM arrayastableindexedtest where string = 'string800'";
